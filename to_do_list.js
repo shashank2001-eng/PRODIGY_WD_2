5601313e127cba37a1b8a99d193dd3ce243ef82a
Shashank Singh <82994997+shashank2001-eng@users.noreply.github.com>
2023-09-21 17:46:04 +0530

    Add files via upload

---
+++ to_do_list.js
@@ -0,0 +1,30 @@
+const inputbox=document.getElementById("input-box");
+const listcontainer=document.getElementById("list-container");
+
+function addtask()
+{
+    if(inputbox.value===''){
+    alert("Write something");
+    }
+else{
+    let li=document.createElement("li");
+    li.innerHTML=inputbox.value;
+    listcontainer.appendChild(li);
+    let span=document.createElement("span");
+    span.innerHTML="\u00d7";
+    li.appendChild(span);
+
+}
+inputbox.value="";
+}
+listcontainer.addEventListener("click",function(e)
+{
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+    }
+},false);
+
+
